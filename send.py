3e294bb05034208954c03dbc645804d93a1272ae
shsssc <10232872+shsssc@users.noreply.github.com>
2021-06-10 15:12:37 -0700

    fixed send.py

--- send.py
+++ send.py
@@ -22,8 +22,7 @@ def get_if():
     return iface
 
 def main():
-
-    if len(sys.argv)<3:
+    if len(sys.argv) < 3:
         print 'pass 2 arguments: <destination> <csv_file_path>'
         exit(1)
 
@@ -38,6 +37,7 @@ def main():
             eth_type = int(row[1], 16)
             # drop packets that are not IPV4_TYPE or IPV6_TYPE
             if eth_type != 0x00000800 and eth_type != 0x000086dd:
+                print('dropped')
                 continue
             ip_proto = int(row[2])
             ip_flags = int(row[3], 16) >> 12
@@ -50,25 +50,32 @@ def main():
             udp_dstport = int(row[10])
             ground_truth = int(row[11])
 
-            pkt = Ether(type=eth_type, src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-            pkt = pkt /IP(dst=addr, proto=ip_proto, flags=ip_flags, len=frame_len, id=ground_truth)
-
+            pkt = None
+            if (ipv6_nxt == -1):
+                pkt = Ether(type=eth_type, src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+                pkt = pkt / IP(dst=addr, proto=ip_proto, flags=ip_flags, len=frame_len, id=ground_truth)
+            elif ipv6_nxt >= 0 and ipv6_nxt <= 255:
+                pkt = Ether(type=eth_type
+                            , src=get_if_hwaddr(iface),
+                            dst='ff:ff:ff:ff:ff:ff') / IPv6(
+                    src='::1',
+                    dst='::1',
+                    nh=ipv6_nxt,
+                    plen=frame_len,
+                    hlim=ground_truth + 1)
+                if ipv6_opt != -1:
+                    jumbo = Jumbo(jumboplen=int(row[5]))
+                    pkt = pkt / IPv6ExtHdrHopByHop(options=jumbo)
 
             if tcp_srcport != -1 and tcp_dstport != -1:
                 pkt = pkt / TCP(dport=tcp_dstport, sport=tcp_srcport, flags=tcp_flags)
             elif udp_srcport != -1 and udp_dstport != -1:
                 pkt = pkt / UDP(dport=udp_dstport, sport=udp_srcport)
-            else:
-                if ipv6_nxt >= 0 and ipv6_nxt <= 255:
-                    pkt = Ether(type=eth_type, src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')/IPv6(src='::1', dst='::1', nh=ipv6_nxt, plen=frame_len, hlim=ground_truth)
-                    if ipv6_opt != -1:
-                        jumbo = Jumbo(jumboplen = int(row[5]))
-                        pkt = pkt / IPv6ExtHdrHopByHop(options=jumbo)
-
+            pkt = pkt / Raw('\0'*(frame_len-len(pkt)))
             pkt.show2()
             sendp(pkt, iface=iface, verbose=False)
-            if(i>=5):
-                break;
+            if (i >= 35):
+                break
 
 
 
